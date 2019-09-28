237cd4ba93461ff7683c1d7b444ddfd0170b5bb0
Obinna Mogbogu <javaobinna@yahoo.com>
2019-09-28 16:13:44 -0500

    Committed code changes to feature

---
+++ CSharpTutorial/ExampleStaticClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleStaticClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Math.Add(10, 20);
+        }
+    }
+}
