cbaffccd215cb06ad1863a20be302fc4dbce9e63
Obinna Mogbogu <javaobinna@yahoo.com>
2019-12-09 23:56:06 -0600

    Committed code to develop. Added Execution4 to Main

---
+++ CSharpTutorial/MSCAChapter1/ParameterizedThread/ParameterThreads.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSCAChapter1.ParameterizedThread
+{
+    class ParameterThreads
+    {
+        public static void PrintHello(object howManyTimes)
+        {
+            Console.WriteLine($"Value entered is {howManyTimes}.");
+            Console.WriteLine($"Now converting value...");
+
+            try
+            {
+                var count = int.Parse(howManyTimes.ToString());
+                Console.WriteLine($"Value converted to int type correctly.");
+                Console.WriteLine($"Now printing Hello message {count} times...");
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine("Hello");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}. Please supply an int type value.");
+            }
+        }
+    }
+}
